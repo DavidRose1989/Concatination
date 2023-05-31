75ac148816fb917336f3230fe1aa5b5b2318788a
DavidRose <DavidRose331@Gmail.com>
2023-05-31 08:26:02 -0500

    initial

---
+++ Concatination/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Concatination
+{
+
+    class Program
+    {
+    static void Main(string[] args) 
+        {
+            string myname = "David";
+
+            string message = "My name is " + myname;
+
+            string capsMessage = message.ToUpper();
+
+            string lowerCaseMessage = message.ToLower();
+
+            Console.WriteLine(lowerCaseMessage);
+            Console.Read();
+
+
+        
+        }
+    }
+}
